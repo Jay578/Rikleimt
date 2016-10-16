d3a0955eaba930d5317952f3ae40573715f1838c
Leah Spontaneo <lspontaneo@riotgames.com>
2016-10-15 21:18:40 -0400

    Updated mySQL settings.

--- config/sequelize.js
+++ config/sequelize.js
@@ -2,12 +2,12 @@ exports.default = {
   sequelize: function(api){
     return {
       "logging"       : true,
-      "database"    : "esports_admin",
+      "database"    : process.env.DBNAME | "rikleimt",
       "dialect"     : "mysql",
       "port"        : 3306,
-      "host"        : "127.0.0.1",
-      "username"    : "root",
-      "password"    : "",
+      "host"        : process.env.DBHOST | "127.0.0.1",
+      "username"    : process.env.DBUSER | "root",
+      "password"    : process.env.DBPASSWORD | "",
       "autoMigrate": false
     };
   }
