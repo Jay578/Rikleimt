8bad57ecdea9b717f443637e6e48072c719fb3aa
Leah Spontaneo <lspontaneo@riotgames.com>
2016-10-15 22:27:20 -0400

    Comment out sequelize plugin and model associations for now.

--- initializers/model-associations.js
+++ initializers/model-associations.js
@@ -1,13 +1,13 @@
-module.exports = {
-  startPriority: 1500,
-  start: function(api, next){
-    var models = Object.keys(api.models);
-    models.forEach(function(model) {
-      if(api.models[model].defineRelationship) {
-        api.models[model].defineRelationship(api);
-      }
-    });
-
-    next();
-  }
-};
+// module.exports = {
+//   startPriority: 1500,
+//   start: function(api, next){
+//     var models = Object.keys(api.models);
+//     models.forEach(function(model) {
+//       if(api.models[model].defineRelationship) {
+//         api.models[model].defineRelationship(api);
+//       }
+//     });
+//
+//     next();
+//   }
+// };
